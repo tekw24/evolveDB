3e21de304d36dbd6daa5833dcc39f0b1c16c8dbc
Torben Eckwert <107031692+tekw24@users.noreply.github.com>
2023-08-31 17:09:00 +0200

    New Rulebase

---
+++ de.thm.mdde.difference.sqlrulebase2/src/de/thm/mdde/difference/sqlrulebase2/RuleBaseProject.java
@@ -0,0 +1,30 @@
+package de.thm.mdde.difference.sqlrulebase2;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.sidiff.editrule.rulebase.project.runtime.library.AbstractRuleBaseProject;
+
+public class RuleBaseProject extends AbstractRuleBaseProject {
+
+	@Override
+	public String getKey() {
+		return "de.thm.mdde.difference.sqlrulebase2";
+	}
+
+	@Override
+	public String getName() {
+		return "Sqlrulebase2 (2023-08-30 11:47:00)";
+	}
+
+	@Override
+	public Set<String> getDocumentTypes() {
+		return Stream.of("http://www.thm.de/mdde").collect(Collectors.toSet());
+	}
+
+	@Override
+	public Set<String> getAttachmentTypes() {
+		return Stream.of("org.sidiff.difference.rulebase.RecognitionRule").collect(Collectors.toSet());
+	}
+}
